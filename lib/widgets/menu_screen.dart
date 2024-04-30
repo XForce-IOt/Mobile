75d7d270e964c16f943fca70cee90fd812d2c7e6
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-04-30 11:09:27 -0500

    Refactor menu_screen.dart to improve menu item selection and display

--- lib/widgets/menu_screen.dart
+++ lib/widgets/menu_screen.dart
@@ -11,7 +11,6 @@ class MenuScreen extends StatefulWidget {
 }
 
 class _MenuScreenState extends State<MenuScreen> {
-
   int selectedMenuIndex = 0;
 
   List<String> menuItems = [
@@ -31,23 +30,34 @@ class _MenuScreenState extends State<MenuScreen> {
   ];
 
   Widget buildMenuRow(int index) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
-      child: Row(
-        children: [
-          Icon(
-            menuIcons[index],
-            color: Colors.white,
-          ),
-          const SizedBox(width: 20.0),
-          Text(
-            menuItems[index],
-            style: const TextStyle(
-                color: Colors.white,
-                fontSize: 20.0,
-                fontWeight: FontWeight.bold),
-          ),
-        ],
+    return InkWell(
+      onTap: () {
+        setState(() {
+          selectedMenuIndex = index;
+        });
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20.0, vertical: 20.0),
+        child: Row(
+          children: [
+            Icon(
+              menuIcons[index],
+              color: selectedMenuIndex == index
+                  ? Colors.white
+                  : Colors.white.withOpacity(0.5),
+            ),
+            const SizedBox(width: 20.0),
+            Text(
+              menuItems[index],
+              style: TextStyle(
+                  color: selectedMenuIndex == index
+                      ? Colors.white
+                      : Colors.white.withOpacity(0.5),
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold),
+            ),
+          ],
+        ),
       ),
     );
   }
@@ -66,20 +76,35 @@ class _MenuScreenState extends State<MenuScreen> {
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const Row(
-                    children: [
-                      CircleAvatar(
-                        radius: 24.0,
-                        backgroundColor: Colors.white,
-                      ),
-                      Text(
-                        'Fredy',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.bold),
-                      )
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20.0, vertical: 20.0),
+                    child: Row(
+                      children: [
+                        const CircleAvatar(
+                          radius: 24.0,
+                          backgroundColor: Colors.white,
+                        ),
+                        const SizedBox(width: 16.0),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            const Text(
+                              'Fredy',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 22.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            Text(
+                              'Dueño de mascotas',
+                              style: TextStyle(
+                                  color: Colors.white.withOpacity(0.5)),
+                            )
+                          ],
+                        )
+                      ],
+                    ),
                   ),
                   Column(
                       mainAxisAlignment: MainAxisAlignment.spaceAround,
@@ -88,35 +113,40 @@ class _MenuScreenState extends State<MenuScreen> {
                           .entries
                           .map((mapEntry) => buildMenuRow(mapEntry.key))
                           .toList()),
-                  const Column(
-                    children: [
-                      Row(
-                        children: [
-                          Icon(FontAwesomeIcons.rightFromBracket,
-                              color: Colors.white),
-                          Text(
-                            'Cerrar Sesión',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          Icon(FontAwesomeIcons.searchengin,
-                              color: Colors.white),
-                          Text(
-                            'Logo',
-                            style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20.0,
-                                fontWeight: FontWeight.bold),
-                          ),
-                        ],
-                      ),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.symmetric(
+                        horizontal: 20.0, vertical: 20.0),
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(FontAwesomeIcons.rightFromBracket,
+                                color: Colors.white.withOpacity(0.5)),
+                            Text(
+                              'Cerrar Sesión',
+                              style: TextStyle(
+                                  color: Colors.white.withOpacity(0.5),
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 20.0),
+                        Row(
+                          children: [
+                            const Icon(FontAwesomeIcons.searchengin,
+                                color: Colors.white),
+                            Text(
+                              'Logo',
+                              style: TextStyle(
+                                  color: Colors.white.withOpacity(0.5),
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   )
                 ],
               ),
