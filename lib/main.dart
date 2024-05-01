f9c5afe32d6d3286565a425b2f19ff0bea0baa54
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-04-30 22:52:56 -0500

    Refactor main.dart to use WelcomeScreen instead of MenuScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,18 @@
 import 'package:flutter/material.dart';
-import 'package:movil/widgets/menu_screen.dart';
+import 'package:movil/screens/welcome_screen.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-Color mainColor = const Color(0xFF2BBCC5);
-Color startingColor = const Color(0xFF2BBCC5);
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'PetHealth',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(primaryColor: mainColor),
-        home: const MenuScreen());
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomeScreen(),
+    );
   }
 }
