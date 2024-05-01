e2a02d1e110a3a98c8a2aa3d2473cc0eea922e5e
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-04-30 23:33:34 -0500

    Refactor welcome_screen.dart to improve UI and layout

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -5,85 +5,96 @@ class WelcomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Material(
-      child: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          children: [
-            const SizedBox(
-              height: 40,
-            ),
-            Align(
-              alignment: Alignment.centerRight,
-              child: TextButton(
-                  onPressed: () {},
-                  child: const Text(
-                    'Omitir',
-                    style: TextStyle(color: Color(0xFF042440), fontSize: 20),
-                  )),
-            ),
-            const SizedBox(
-              height: 100,
-            ),
-            Padding(
-                padding: const EdgeInsets.all(20),
-                child: Image.asset('assets/images/logo.png')),
-            const SizedBox(height: 5),
-            const Text(
-              '"Mejor Cuidado, Mascotas Felices"',
-              style: TextStyle(
-                color: Color.fromARGB(164, 4, 36, 64),
-                fontSize: 20,
-              ),
-            ),
-            const SizedBox(
-              height: 80,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Image.asset('assets/images/top-auth.png',
+                width: MediaQuery.of(context).size.width / 1.2,
+                fit: BoxFit.cover),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Material(
-                  color: const Color(0xFF2BBCC5),
-                  borderRadius: BorderRadius.circular(10),
-                  child: InkWell(
-                    onTap: () {},
-                    child: const Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 40),
-                      child: Text(
-                        'Log In',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
+                Align(
+                  alignment: Alignment.centerRight,
+                  child: TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        'Omitir',
+                        style:
+                            TextStyle(color: Color(0xFF042440), fontSize: 20),
+                      )),
+                ),
+                Padding(
+                    padding: const EdgeInsets.all(20),
+                    child: Image.asset('assets/images/logo.png')),
+                const Text(
+                  '"Mejor Cuidado, Mascotas Felices"',
+                  style: TextStyle(
+                    color: Color.fromARGB(164, 4, 36, 64),
+                    fontSize: 20,
                   ),
                 ),
-                Material(
-                  color: const Color(0xFF2BBCC5),
-                  borderRadius: BorderRadius.circular(10),
-                  child: InkWell(
-                    onTap: () {},
-                    child: const Padding(
-                      padding:
-                          EdgeInsets.symmetric(vertical: 15, horizontal: 40),
-                      child: Text(
-                        'Sign Up',
-                        style: TextStyle(
-                            color: Colors.white,
-                            fontSize: 20,
-                            fontWeight: FontWeight.bold),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    Material(
+                      color: const Color(0xFF2BBCC5),
+                      borderRadius: BorderRadius.circular(10),
+                      child: InkWell(
+                        onTap: () {},
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 15, horizontal: 40),
+                          child: Text(
+                            'Log In',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
                       ),
                     ),
-                  ),
-                )
+                    Material(
+                      color: const Color(0xFF2BBCC5),
+                      borderRadius: BorderRadius.circular(10),
+                      child: InkWell(
+                        onTap: () {},
+                        child: const Padding(
+                          padding: EdgeInsets.symmetric(
+                              vertical: 15, horizontal: 40),
+                          child: Text(
+                            'Sign Up',
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold),
+                          ),
+                        ),
+                      ),
+                    )
+                  ],
+                ),
               ],
-            )
-          ],
-        ),
+            ),
+          ),
+          Positioned(
+            bottom: 0,
+            right: 0,
+            child: Image.asset(
+              'assets/images/bottom-auth.png',
+              width: MediaQuery.of(context).size.width / 2,
+              fit: BoxFit.cover,
+            ),
+          )
+        ],
       ),
     );
   }
