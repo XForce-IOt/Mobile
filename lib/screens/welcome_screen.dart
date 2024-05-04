d75833fdc0b77b7c02786e549b818287077a5572
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-05-03 22:54:01 -0500

    Refactor welcome_screen.dart to add login functionality

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:movil/screens/login_screen.dart';
 
 class WelcomeScreen extends StatelessWidget {
   const WelcomeScreen({super.key});
@@ -48,7 +49,13 @@ class WelcomeScreen extends StatelessWidget {
                       color: const Color(0xFF2BBCC5),
                       borderRadius: BorderRadius.circular(10),
                       child: InkWell(
-                        onTap: () {},
+                        onTap: () {
+                          Navigator.push(context, MaterialPageRoute(
+                            builder: (context) {
+                              return const LoginScreen();
+                            },
+                          ));
+                        },
                         child: const Padding(
                           padding: EdgeInsets.symmetric(
                               vertical: 15, horizontal: 40),
