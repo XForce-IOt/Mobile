aa4198e9412e3aa33d39e18309372c84712237a7
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-05-06 18:17:53 -0500

    Refactor welcome_screen.dart to fix navigation bug, add login functionality, and improve UI and layout

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoginScreenState createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  bool passToggle = true;
+
+  @override
+  Widget build(BuildContext context) {
+    var isKeyboardOpen = MediaQuery.of(context).viewInsets.bottom != 0;
+
+    return Scaffold(
+      body: SingleChildScrollView(
+        physics: const AlwaysScrollableScrollPhysics(),
+        child: ConstrainedBox(
+          constraints: BoxConstraints(
+            minHeight: MediaQuery.of(context).size.height,
+          ),
+          child: IntrinsicHeight(
+            child: Stack(
+              children: <Widget>[
+                Positioned(
+                  top: 0,
+                  left: 0,
+                  child: Image.asset('assets/images/top-auth.png',
+                      width: MediaQuery.of(context).size.width / 1.2,
+                      fit: BoxFit.cover),
+                ),
+                Padding(
+                  padding: const EdgeInsets.all(10),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(10),
+                        child: Image.asset(
+                          'assets/images/logo_header.png',
+                          width: MediaQuery.of(context).size.width * 0.4,
+                          fit: BoxFit.cover,
+                        ),
+                      ),
+                      const SizedBox(height: 15),
+                      const Text(
+                        'Login',
+                        style: TextStyle(
+                            color: Color(0xFF042440),
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: TextField(
+                          decoration: InputDecoration(
+                            label: const Text('Username'),
+                            hintStyle:
+                                const TextStyle(color: Color(0xFF042440)),
+                            prefixIcon: const Icon(Icons.person),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: TextField(
+                          obscureText: passToggle,
+                          decoration: InputDecoration(
+                            label: const Text('Password'),
+                            hintStyle:
+                                const TextStyle(color: Color(0xFF042440)),
+                            prefixIcon: const Icon(Icons.lock),
+                            suffixIcon: InkWell(
+                              onTap: () {
+                                setState(() {
+                                  passToggle = !passToggle;
+                                });
+                              },
+                              child: Icon(passToggle
+                                  ? Icons.visibility
+                                  : Icons.visibility_off),
+                            ),
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.circular(10),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(5.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.end,
+                          children: [
+                            TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'Forgot Password?',
+                                style: TextStyle(
+                                    color: Color(0xFF042440),
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.w500),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(10.0),
+                        child: SizedBox(
+                          width: double.infinity,
+                          child: Material(
+                            color: const Color(0xFF2BBCC5),
+                            borderRadius: BorderRadius.circular(10),
+                            child: InkWell(
+                              onTap: () {},
+                              child: const Center(
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(
+                                      vertical: 15, horizontal: 40),
+                                  child: Text(
+                                    'Log In',
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 20,
+                                        fontWeight: FontWeight.bold),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const Text(
+                            'Don\'t have an account? ',
+                            style: TextStyle(
+                                color: Color(0xFF042440),
+                                fontSize: 16,
+                                fontWeight: FontWeight.w500),
+                          ),
+                          TextButton(
+                              onPressed: () {},
+                              child: const Text(
+                                'Create Account',
+                                style: TextStyle(
+                                    color: Color(0xFF2BBCC5),
+                                    fontSize: 16,
+                                    fontWeight: FontWeight.bold),
+                              ))
+                        ],
+                      )
+                    ],
+                  ),
+                ),
+                Positioned(
+                  bottom: 0,
+                  right: 0,
+                  child: isKeyboardOpen
+                      ? Container()
+                      : Image.asset(
+                          'assets/images/bottom-auth.png',
+                          width: MediaQuery.of(context).size.width / 2,
+                          fit: BoxFit.cover,
+                        ),
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
