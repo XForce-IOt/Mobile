b223e5348cb3ef41cb70c604009851b12f7f2001
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-05-06 19:50:36 -0500

    Refactor navbar_roots.dart to implement bottom navigation bar with multiple screens

---
+++ lib/widgets/navbar_roots.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+import 'package:movil/screens/home_screen.dart';
+
+class NavBarRoots extends StatefulWidget {
+  const NavBarRoots({super.key});
+
+  @override
+  State<NavBarRoots> createState() => _NavBarRootsState();
+}
+
+class _NavBarRootsState extends State<NavBarRoots> {
+  int _selectedIndex = 0;
+
+  final _screens = [const HomeScreen(), Container(), Container(), Container()];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: _screens[_selectedIndex],
+        bottomNavigationBar: SizedBox(
+          height: 80,
+          child: BottomNavigationBar(
+            backgroundColor: Colors.white,
+            type: BottomNavigationBarType.fixed,
+            selectedItemColor: const Color(0xFF042440),
+            unselectedItemColor: const Color(0xFF2BBCC5),
+            selectedLabelStyle: const TextStyle(
+              fontSize: 15,
+              fontWeight: FontWeight.bold,
+            ),
+            currentIndex: _selectedIndex,
+            onTap: (index) {
+              setState(() {
+                _selectedIndex = index;
+              });
+            },
+            items: const [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.home),
+                label: 'Home',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.calendar_today),
+                label: 'Appointments',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.person),
+                label: 'Profile',
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.settings),
+                label: 'Settings',
+              ),
+            ],
+          ),
+        ));
+  }
+}
