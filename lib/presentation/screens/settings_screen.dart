e58e9a9382c33dc507351f56c2ff2fcf2d576871
SaulMendozaB <U20211C677@upc.edu.pe>
2024-06-02 21:14:05 -0500

    settings screen
    settings screen

---
+++ lib/presentation/screens/settings_screen.dart
@@ -0,0 +1,159 @@
+import 'package:flutter/material.dart';
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Settings',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MySettingsScreen(),
+    );
+  }
+}
+
+class MySettingsScreen extends StatefulWidget {
+  @override
+  _MySettingsScreenState createState() => _MySettingsScreenState();
+}
+
+class _MySettingsScreenState extends State<MySettingsScreen> {
+  final _formKey = GlobalKey<FormState>();
+  String _currentPassword = '';
+  String _newPassword = '';
+  String _repeatNewPassword = '';
+
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+      print('CurrentPassword: $_currentPassword');
+      print('NewPassword: $_newPassword');
+      print('RepeatNewPassword: $_repeatNewPassword');
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(content: Text('Form submitted successfully!')),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final Color customColor = Color(0xFF2BBCC5);
+    return Scaffold(
+      appBar: AppBar(
+        title: Container(
+          padding: const EdgeInsets.all(8.0),
+          child: Text('Password',
+            style: TextStyle(
+              color: Colors.white, // Color del texto del título
+              fontWeight: FontWeight.bold,
+            ),),
+        ),
+        centerTitle: true, // Centrar el título en la AppBar
+        backgroundColor: customColor, // Fondo transparente para que el contenedor sea visible
+        elevation: 0, // Sin sombra
+      ),
+      body: Center(
+        child: SingleChildScrollView(
+          padding: const EdgeInsets.all(16.0),
+          child: Form(
+            key: _formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Icon(
+                  Icons.person,
+                  size: 100,
+                  color: customColor,
+                ),
+                SizedBox(height: 5),
+                Text(
+                  'Saúl Mendoza',
+                  style: TextStyle(
+                    fontSize: 18,
+                    //fontWeight: FontWeight.bold,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  width: 250,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Current password',
+                    ),
+                    obscureText: true, // Ocultar el texto de la contraseña
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter your current password';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _currentPassword = value!;
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  width: 250,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'New password',
+                    ),
+                    obscureText: true, // Ocultar el texto de la contraseña
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter a new password';
+                      }
+                      if (value == _currentPassword){
+                        return 'The new password cant be the same as the current';
+                      }
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _newPassword = value!;
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                SizedBox(
+                  width: 250,
+                  child: TextFormField(
+                    decoration: InputDecoration(
+                      border: OutlineInputBorder(),
+                      labelText: 'Confirm the new password',
+                    ),
+                    obscureText: true, // Ocultar el texto de la contraseña
+                    validator: (value) {
+                      if (value == null || value.isEmpty) {
+                        return 'Please enter the new password again';
+                      }
+                      /*if (value != _newPassword){
+                        return 'Does not match new password';
+                      }*/
+                      return null;
+                    },
+                    onSaved: (value) {
+                      _repeatNewPassword = value!;
+                    },
+                  ),
+                ),
+                SizedBox(height: 20),
+                ElevatedButton(
+                  onPressed: _submitForm,
+                  style: ElevatedButton.styleFrom(
+                    foregroundColor: Colors.white, backgroundColor: customColor,
+                  ),
+                  child: Text('Save'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
