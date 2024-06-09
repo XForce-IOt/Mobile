73bd7536fefb5b3a4d2c7581ef69a0798ba5894d
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-08 21:12:28 -0500

    Refactor navigation routes to use const for screen widgets

--- lib/presentation/screens/home_data/gps_screen.dart
+++ lib/presentation/screens/home_data/gps_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_final_fields
+
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:movil/presentation/screens/home_data/heart_rate_screen.dart';
@@ -14,7 +16,7 @@ class GpsScreen extends StatefulWidget {
 }
 
 class _GpsScreenState extends State<GpsScreen> {
-  final Color customColor = Color(0xFF2BBCC5);
+  final Color customColor = const Color(0xFF2BBCC5);
   final String userName = "John Doe"; // Nombre del usuario
   final String petName = "Vaguito"; // Nombre de la mascota
   final String petBreed = "Golden Retriever"; // Raza de la mascota
@@ -33,14 +35,14 @@ class _GpsScreenState extends State<GpsScreen> {
           children: [
             Row(
               children: [
-                Icon(
+                const Icon(
                   Icons.person,
                   color: Colors.white,
                 ),
-                SizedBox(width: 8),
+                const SizedBox(width: 8),
                 Text(
                   userName,
-                  style: TextStyle(
+                  style: const TextStyle(
                     color: Colors.white,
                     fontSize: 18,
                     fontWeight: FontWeight.bold,
@@ -49,12 +51,14 @@ class _GpsScreenState extends State<GpsScreen> {
               ],
             ),
             IconButton(
-              icon: Icon(Icons.notifications),
+              icon: const Icon(Icons.notifications),
               color: Colors.white,
               onPressed: () {
                 // Acción del botón de notificaciones
                 ScaffoldMessenger.of(context).showSnackBar(
-                  SnackBar(content: Text('Notification button pressed')),
+                  const SnackBar(
+                    content: Text('Notification button pressed'),
+                  ),
                 );
               },
             ),
@@ -74,13 +78,13 @@ class _GpsScreenState extends State<GpsScreen> {
                     size: 50,
                     color: customColor,
                   ),
-                  SizedBox(width: 16),
+                  const SizedBox(width: 16),
                   Column(
                     crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
                         petName,
-                        style: TextStyle(
+                        style: const TextStyle(
                           fontSize: 18,
                           fontWeight: FontWeight.bold,
                         ),
@@ -98,8 +102,8 @@ class _GpsScreenState extends State<GpsScreen> {
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 16.0),
             child: Align(
               alignment: Alignment.centerLeft, // Alinea el texto a la izquierda
               child: Text(
@@ -115,7 +119,7 @@ class _GpsScreenState extends State<GpsScreen> {
           Container(
             margin: const EdgeInsets.symmetric(vertical: 16.0),
             padding: const EdgeInsets.all(16.0),
-            constraints: BoxConstraints(
+            constraints: const BoxConstraints(
               maxWidth: 350, // Ajusta el ancho máximo aquí
               maxHeight: 250,
             ),
@@ -133,9 +137,11 @@ class _GpsScreenState extends State<GpsScreen> {
             child: Row(
               children: [
                 _buildElevatedButton(Icons.favorite, 'Heart Rate', context),
-                _buildElevatedButton(Icons.directions_run, 'Activity Physics', context),
+                _buildElevatedButton(
+                    Icons.directions_run, 'Activity Physics', context),
                 _buildElevatedButton(Icons.thermostat, 'Temperature', context),
-                _buildElevatedButton(Icons.brightness_2, 'Sleep Quality', context),
+                _buildElevatedButton(
+                    Icons.brightness_2, 'Sleep Quality', context),
                 _buildElevatedButton(Icons.local_drink, 'Hydration', context),
               ],
             ),
@@ -146,18 +152,20 @@ class _GpsScreenState extends State<GpsScreen> {
   }
 
   Widget _buildGoogleMap() {
-      return GoogleMap(
-        onMapCreated: (controller) {
-          _controller = controller;
-        },
-        onTap: _addMarker,
-        initialCameraPosition: CameraPosition(
-          target: LatLng(-12.0464, -77.0428), // Coordenadas de ejemplo (Lima, Perú)
-          zoom: 14,
-        ),
-        markers: _markers,
-      );
+    return GoogleMap(
+      onMapCreated: (controller) {
+        _controller = controller;
+      },
+      onTap: _addMarker,
+      initialCameraPosition: CameraPosition(
+        target:
+            LatLng(-12.0464, -77.0428), // Coordenadas de ejemplo (Lima, Perú)
+        zoom: 14,
+      ),
+      markers: _markers,
+    );
   }
+
   void _addMarker(LatLng position) {
     setState(() {
       _markers.clear();
@@ -170,7 +178,8 @@ class _GpsScreenState extends State<GpsScreen> {
     });
   }
 
-  Widget _buildElevatedButton(IconData icon, String label, BuildContext context) {
+  Widget _buildElevatedButton(
+      IconData icon, String label, BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(8.0),
       child: ElevatedButton(
@@ -178,11 +187,13 @@ class _GpsScreenState extends State<GpsScreen> {
           _navigateToScreen(label, context);
         },
         style: ElevatedButton.styleFrom(
-          padding: EdgeInsets.symmetric(vertical: 16.0, horizontal: 14.0), // Ajusta el tamaño del botón
+          padding: const EdgeInsets.symmetric(
+              vertical: 16.0, horizontal: 14.0), // Ajusta el tamaño del botón
           backgroundColor: Colors.white,
           side: BorderSide(color: customColor),
           shape: RoundedRectangleBorder(
-            borderRadius: BorderRadius.circular(10.0), // Ajusta la esquina del botón
+            borderRadius:
+                BorderRadius.circular(10.0), // Ajusta la esquina del botón
           ),
         ),
         child: Column(
@@ -192,7 +203,7 @@ class _GpsScreenState extends State<GpsScreen> {
               icon,
               color: customColor,
             ),
-            SizedBox(height: 8),
+            const SizedBox(height: 8),
             Text(
               label,
               style: TextStyle(
@@ -204,34 +215,36 @@ class _GpsScreenState extends State<GpsScreen> {
       ),
     );
   }
+
   void _navigateToScreen(String label, BuildContext context) {
     switch (label) {
       case 'Heart Rate':
         Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => HeartRateScreen()),
+          MaterialPageRoute(builder: (context) => const HeartRateScreen()),
         );
         break;
       case 'Activity Physics':
         Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => ActivityPhysicsScreen()),
+          MaterialPageRoute(
+              builder: (context) => const ActivityPhysicsScreen()),
         );
         break;
       case 'Hydration':
         Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => HydrationScreen()),
+          MaterialPageRoute(builder: (context) => const HydrationScreen()),
         );
         break;
       case 'Sleep Quality':
         Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => SleepQualityScreen()),
+          MaterialPageRoute(builder: (context) => const SleepQualityScreen()),
         );
         break;
       case 'Temperature':
         Navigator.of(context).push(
-          MaterialPageRoute(builder: (context) => TemperatureScreen()),
+          MaterialPageRoute(builder: (context) => const TemperatureScreen()),
         );
         break;
-    // Agrega más casos para las demás opciones de botón...
+      // Agrega más casos para las demás opciones de botón...
       default:
       // Manejar caso por defecto
     }
