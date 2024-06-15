9c070d3c9184be904ef66dc6d65a965571b8665e
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-14 22:02:41 -0500

    Refactor clinic_card.dart to optimize image loading and update UI layout

--- lib/application/appointment_function/ui/clinic_card.dart
+++ lib/application/appointment_function/ui/clinic_card.dart
@@ -1,7 +1,4 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-import 'package:flutter/widgets.dart';
 import 'package:flutter_rating_stars/flutter_rating_stars.dart';
 
 class ClinicCard extends StatelessWidget {
@@ -24,11 +21,12 @@ class ClinicCard extends StatelessWidget {
               children: <Widget>[
                 ListTile(
                   leading: ConstrainedBox(
-                      constraints: BoxConstraints(maxHeight: 70, maxWidth: 70),
+                      constraints:
+                          const BoxConstraints(maxHeight: 70, maxWidth: 70),
                       child: Image.network(
                           'https://images.pexels.com/photos/5733422/pexels-photo-5733422.jpeg?auto=compress&cs=tinysrgb&w=600',
                           fit: BoxFit.cover)),
-                  title: Text("Dulces patitas titulo"),
+                  title: const Text("Dulces patitas titulo"),
                   subtitle: const RatingStars(
                     axis: Axis.horizontal,
                     value: 4.5,
@@ -38,11 +36,11 @@ class ClinicCard extends StatelessWidget {
                     maxValue: 5,
                   ),
                 ),
-                Text("Dirección larga"),
-                Wrap(
+                const Text("Dirección larga"),
+                const Wrap(
                     crossAxisAlignment: WrapCrossAlignment.center,
                     children: [Icon(Icons.phone), Text("999999999")]),
-                Wrap(
+                const Wrap(
                     crossAxisAlignment: WrapCrossAlignment.center,
                     children: [Icon(Icons.facebook), Text("Dulces patitas")])
               ],
