f88ab9cfd0d7482e24a00219f1d98deb487d2088
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-27 09:01:29 -0500

    feat: Update User model to include image field

--- lib/shared/model/user_model.dart
+++ lib/shared/model/user_model.dart
@@ -1,37 +1,34 @@
 class User {
   final int id;
   final String name;
-  final String lastname;
-  final String email;
-  final String password;
-  final String repassword;
-  final String newpassword;
+  final String lastName;
   final String address;
   final String phone;
+  final String email;
+  final String password;
+  final String image;
 
   User({
     required this.id,
     required this.name,
-    required this.lastname,
-    required this.email,
-    required this.password,
-    required this.repassword,
-    required this.newpassword,
+    required this.lastName,
     required this.address,
     required this.phone,
+    required this.email,
+    required this.password,
+    required this.image,
   });
 
   factory User.fromJson(Map<String, dynamic> json) {
     return User(
       id: json['id'],
       name: json['name'],
-      lastname: json['lastname'],
-      email: json['email'],
-      password: json['password'],
-      repassword: json['repassword'],
-      newpassword: json['newpassword'],
+      lastName: json['lastName'],
       address: json['address'],
       phone: json['phone'],
+      email: json['email'],
+      password: json['password'],
+      image: json['image'],
     );
   }
 
@@ -39,13 +36,12 @@ class User {
     return {
       'id': id,
       'name': name,
-      'lastname': lastname,
-      'email': email,
-      'password': password,
-      'repassword': repassword,
-      'newpassword': newpassword,
+      'lastName': lastName,
       'address': address,
       'phone': phone,
+      'email': email,
+      'password': password,
+      'image': image,
     };
   }
 }
