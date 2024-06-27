97568c47ab09c056053dd924c5ac5a0848d71f7e
Fredy Antonio Almeyda Alania <almeydaalania@gmail.com>
2024-06-26 22:17:52 -0500

    chore: Update main.dart to load environment variables with flutter_dotenv

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:movil/presentation/screens/welcome_screen.dart';
 
-void main() {
+Future<void> main() async {
+  await dotenv.load(fileName: ".env");
+
   runApp(const MyApp());
 }
 
