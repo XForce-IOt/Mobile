19bab14ebe1b1a3b39786c686261ce36f336ed6e
Emerson <emersonquispe2808@gmail.com>
2024-06-27 19:45:40 -0500

    update screen home

--- lib/presentation/screens/home_screen.dart
+++ lib/presentation/screens/home_screen.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:percent_indicator/circular_percent_indicator.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:movil/shared/model/user_model.dart';
+import 'package:provider/provider.dart';
+import '../../shared/model/pet_model.dart';
+import '../../shared/services/pet_service.dart';
+import '../widgets/home/carousel_widget.dart';
+import '../widgets/home/tab_metrics_widget.dart';
+import '../widgets/home/widget_selector_widget.dart';
 
 class HomeScreen extends StatelessWidget {
   final User user;
@@ -31,12 +35,22 @@ class MyHomeScreen extends StatefulWidget {
 class _MyHomeScreenState extends State<MyHomeScreen> {
   int selectedPetIndex = 0;
   int selectedMetricIndex = -1;
+  List<Pet> pets = [];
+  bool isLoading = true;
+  int petOwnerId = 1;
+  //widget.user.id.toString()
 
-  final List<Pet> pets = [
-    Pet(name: 'Fido', type: 'Dog', age: 3, description: 'Friendly dog'),
-    Pet(name: 'Whiskers', type: 'Cat', age: 2, description: 'Curious cat'),
-    Pet(name: 'Tweety', type: 'Bird', age: 1, description: 'Chirpy bird')
-  ];
+  @override
+  void initState() {
+    super.initState();
+    final petsProvider = Provider.of<PetService>(context, listen: false);
+    petsProvider.getPets(petOwnerId).then((_) {
+      setState(() {
+        pets = petsProvider.pets;
+        isLoading = false;
+      });
+    });
+  }
 
   void onMetricSelected(int index) {
     setState(() {
@@ -246,330 +260,4 @@ class _MyHomeScreenState extends State<MyHomeScreen> {
       ),
     );
   }
-}
-
-class TabMetrics extends StatelessWidget {
-  final IconData icon;
-  final String name;
-  final VoidCallback onTap;
-
-  const TabMetrics(
-      {super.key, required this.icon, required this.name, required this.onTap});
-  @override
-  Widget build(BuildContext context) {
-    return GestureDetector(
-      onTap: onTap,
-      child: Container(
-        decoration: BoxDecoration(
-          color: Colors.white,
-          borderRadius: BorderRadius.circular(15.0),
-        ),
-        margin: const EdgeInsets.all(5.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Icon(icon, size: 60.0, color: const Color(0xFF2BBCC5)),
-            const SizedBox(height: 7.0),
-            Text(
-              name,
-              textAlign: TextAlign.center,
-              style: GoogleFonts.workSans(
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  color: Colors.black),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class Carousel extends StatefulWidget {
-  final List<Pet> pets;
-  final Function(int) onPageChanged;
-
-  const Carousel({
-    super.key,
-    required this.pets,
-    required this.onPageChanged,
-  });
-
-  @override
-  State<Carousel> createState() => _CarouselState();
-}
-
-class _CarouselState extends State<Carousel> {
-  int selectedIndex = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        SizedBox(
-            height: 150,
-            width: double.infinity,
-            child: PageView.builder(
-              controller: PageController(viewportFraction: 0.8),
-              itemCount: widget.pets.length,
-              onPageChanged: (index) {
-                setState(() {
-                  selectedIndex = index;
-                });
-                widget.onPageChanged(index);
-              },
-              itemBuilder: (context, index) {
-                return Card(
-                  elevation: 10.0,
-                  shape: RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(30.0),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.only(left: 20),
-                    child: Row(
-                      children: [
-                        const Icon(
-                          Icons.pets,
-                          size: 70,
-                          color: Colors.black,
-                        ),
-                        const SizedBox(width: 60),
-                        Padding(
-                          padding: const EdgeInsets.symmetric(vertical: 40),
-                          child: Column(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              children: [
-                                Text(
-                                  widget.pets[index].name,
-                                  style: const TextStyle(
-                                    fontSize: 18,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                ),
-                                Text(
-                                  widget.pets[index].type,
-                                  style: TextStyle(
-                                    fontSize: 16,
-                                    color: Colors.grey[600],
-                                  ),
-                                ),
-                              ]),
-                        ),
-                      ],
-                    ),
-                  ),
-                );
-              },
-            )),
-        //segunda parte
-      ],
-    );
-  }
-}
-
-class CardInfo extends StatelessWidget {
-  final Pet pet;
-  const CardInfo({super.key, required this.pet});
-
-  @override
-  Widget build(BuildContext context) {
-    return Card(
-      child: Padding(
-        padding: const EdgeInsets.only(left: 15),
-        child: Row(
-          children: [
-            const Icon(
-              Icons.pets,
-              size: 50,
-              color: Colors.black,
-            ),
-            const SizedBox(width: 50),
-            Center(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    pet.name,
-                    style: const TextStyle(
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  Text(
-                    pet.type,
-                    style: TextStyle(
-                      fontSize: 16,
-                      color: Colors.grey[600],
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
-    );
-  }
-}
-
-class ProgressIndicator extends StatelessWidget {
-  final double current;
-  final double max;
-
-  const ProgressIndicator({
-    super.key,
-    required this.current,
-    required this.max,
-  });
-
-  @override
-  Widget build(BuildContext context) {
-    double percent = current / max;
-
-    return CircularPercentIndicator(
-      radius: 100.0,
-      lineWidth: 10.0,
-      percent: percent,
-      center: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text(
-            '${(percent * 100).toStringAsFixed(1)}%',
-            style: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-          ),
-          Text(
-            '$current / $max cal',
-            style: const TextStyle(fontSize: 16.0),
-          ),
-        ],
-      ),
-      progressColor: Colors.blue,
-      backgroundColor: Colors.grey.shade300,
-      circularStrokeCap: CircularStrokeCap.round,
-    );
-  }
-}
-
-class WidgetSelector extends StatefulWidget {
-  final String word;
-
-  const WidgetSelector({super.key, required this.word});
-
-  @override
-  State<WidgetSelector> createState() => _WidgetSelectorState();
-}
-
-class _WidgetSelectorState extends State<WidgetSelector> {
-  GoogleMapController? _controller;
-  Set<Marker> _markers = {};
-
-  void _addMarker(LatLng position) {
-    setState(() {
-      _markers.clear();
-      _markers.add(
-        Marker(
-          markerId: MarkerId(position.toString()),
-          position: position,
-        ),
-      );
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    if (widget.word == "Heart Rate") {
-      return heartRate(84);
-    } else if (widget.word == 'Activity Physics') {
-      return activity(90);
-    } else if (widget.word == 'Temperature') {
-      return thermostat(35);
-    } else if (widget.word == 'Sleep Quality') {
-      return bedTime(70);
-    } else if (widget.word == 'GPS') {
-      return gps();
-    } else {
-      return hydration(70);
-    }
-  }
-
-  Widget heartRate(double metric) {
-    return Column(
-      children: [
-        const Icon(Icons.monitor_heart_outlined, size: 140, color: Colors.red),
-        Text(
-          "$metric BPM",
-          style: GoogleFonts.workSans(
-            fontSize: 40,
-            fontWeight: FontWeight.w500,
-            color: const Color(0xFF042440),
-          ),
-        )
-      ],
-    );
-  }
-
-  Widget activity(double currentCalories) {
-    return ProgressIndicator(current: currentCalories, max: 200);
-  }
-
-  Widget thermostat(double metric) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Icon(Icons.thermostat_auto_rounded, size: 140, color: Colors.red),
-        Text(
-          "$metric °C",
-          style: GoogleFonts.workSans(
-            fontSize: 40,
-            fontWeight: FontWeight.w500,
-            color: const Color(0xFF042440),
-          ),
-        )
-      ],
-    );
-  }
-
-  Widget bedTime(double currentBed) {
-    return ProgressIndicator(current: currentBed, max: 100);
-  }
-
-  Widget gps() {
-    return Container(
-      constraints: const BoxConstraints(
-        maxWidth: 350,
-        maxHeight: 250,
-      ),
-      child: GoogleMap(
-        onMapCreated: (controller) {
-          _controller = controller;
-        },
-        onTap: _addMarker,
-        initialCameraPosition: const CameraPosition(
-          target: LatLng(-12.0464, -77.0428),
-          zoom: 14,
-        ),
-        markers: _markers,
-      ),
-    );
-  }
-
-  Widget hydration(double currentHydration) {
-    return ProgressIndicator(current: currentHydration, max: 100);
-  }
-}
-
-class Pet {
-  final String name;
-  final String type;
-  //final String sex;
-  final int age;
-  //final String size;
-  //final Double perimeter;
-  final String description;
-
-  Pet(
-      {required this.name,
-      required this.type,
-      required this.age,
-      required this.description});
 }
